131318d6ebab6d169dfb7b58e9fc632e500e8c6e
am16br <65733333+am16br@users.noreply.github.com>
2020-07-10 14:20:40 -0400

    Update app.py

--- Website/app.py
+++ Website/app.py
@@ -8,6 +8,11 @@
 #Line used to enter python env in windows cmd
 
 from flask import *
+import os
+import datetime as dt
+from datetime import date
+import pandas as pd
+import pandas_datareader.data as web
 
 app = Flask(__name__)
 #Default port is '127.0.0.1:5000'
@@ -15,6 +20,13 @@ app = Flask(__name__)
 #takes you to the home page
 @app.route('/')                                                 #home page
 def home():
+    csvname = ('^GSPC.csv')         #naming for s&P500
+    if os.path.exists(csvname):     #removing if it exists
+        os.remove(csvname)
+    endDate = date.today()
+    startDate = date(endDate.year - 5, endDate.month, endDate.day)
+    df = web.DataReader(^GSPC, 'yahoo', startDate, endDate) #reading open, high, low, close, and volume daily for S&P500 for last 5 years
+    df.to_csv(csvname)                              #dding dt to csv
     return render_template('index.html')
 
 #if index is typed directly it redirects to '/'
