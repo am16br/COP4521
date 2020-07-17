c8d82f162d5b03888d4ca9443431cf282779aa9b
Xander0070 <50340350+Xander0070@users.noreply.github.com>
2020-07-17 15:31:15 -0400

    Working table based on database
    The website will now populate a table on the home page that will display the data within the database

--- Website/app.py
+++ Website/app.py
@@ -1,3 +1,9 @@
+#########################################################################
+# Alexander Marcano, Aidan Martin, Douglas Kendall
+# due 07/30/2020
+# The project is the cumulative work of everyone above
+######################################################################
+
 from flask import *
 import os
 import datetime as dt
@@ -7,7 +13,10 @@ import pandas_datareader.data as web
 import sqlite3  #importing sqlite3
 import csv
 
-app = Flask(__name__)
+#python-env\Scripts\activate.bat
+#Line used to enter python env in windows cmd
+
+app = Flask(__name__, instance_relative_config=True)
 #Default port is '127.0.0.1:5000'
 
 #takes you to the home page
@@ -15,9 +24,11 @@ app = Flask(__name__)
 def home():
     ticker = '^GSPC'
     csvname = (ticker + '.csv')
-    dbname = (ticker + '.db')
+    dbname = ('Projecto.db')
+
     if os.path.exists(csvname):
       os.remove(csvname)
+
     #years = int(input("Enter number of years to check stock data: "))
     endDate = date.today()
     startDate = date(endDate.year - 5, endDate.month, endDate.day)
@@ -25,53 +36,52 @@ def home():
     df = web.DataReader(ticker, 'yahoo', startDate, endDate)
     df.to_csv(csvname)
  
-    conn = sqlite3.connect(dbname) #connecting to/creating/opening database
+    con = sqlite3.connect(dbname)                   #connecting to/creating/opening database
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()                              #setting cursor
 
-    cur = conn.cursor()                     #setting cursor
-    cur.execute('''CREATE TABLE IF NOT EXISTS Stock (
-                        Date DATE,
-                        High FLOAT,
-                        Low FLOAT,
-                        Open FLOAT,
-                        Close FLOAT,
-                        Volume FLOAT,
-                        AdjClose FLOAT);''')
-    conn.close()
+    #Delete old table, to avoid duplicates
+    cur.execute("""DELETE FROM Stock""")
 
-    with sqlite3.connect(dbname) as con:  #connecting database
-       cur = con.cursor()
-       cur.execute("""DELETE FROM Stock""")
-       with open(csvname) as csv_file:
-           csv_reader = csv.reader(csv_file, delimiter=',')
-           line_count = 0
-           for row in csv_reader:
-               Date = row[0]
-               High = row[1]
-               Low = row[2]
-               Open = row[3]
-               Close = row[4]
-               Volume = row[5]
-               AdjClose = row[6]
-               cur.execute("""INSERT INTO Stock
-                           (Date, High, Low , Open, Close, Volume, AdjClose)
-                           VALUES (?, ?, ?, ?, ?, ?, ?);""", (Date, High, Low, Open, Close, Volume, AdjClose))
-    return render_template('index.html')
+    cur.execute('CREATE TABLE IF NOT EXISTS Stock(Date TEXT, High REAL, Low REAL, Open REAL, Close REAL, Volume REAL, AdjClose REAL);')
+
+    try:
+        with open(csvname) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                Date = row[0]
+                High = row[1]
+                Low = row[2]
+                Open = row[3]
+                Close = row[4]
+                Volume = row[5]
+                AdjClose = row[6]
+                cur.execute("""INSERT INTO Stock(Date, High, Low , Open, Close, Volume, AdjClose)
+                            VALUES (?, ?, ?, ?, ?, ?, ?);""", (Date, High, Low, Open, Close, Volume, AdjClose))
+                con.commit()
+
+        cur.execute('SELECT * FROM Stock')
+        rows = cur.fetchall();
+    except:
+        con.rollback()                                      #in the event of an error rollback database
+        print("error in insert operation")
+    finally:
+        return render_template('index.html', rows=rows)
 
 #if index is typed directly it redirects to '/'
 @app.route('/index', methods=['GET'])                           #takes you to the route
 def index():
     return redirect(url_for('home'))
 
-#takes you to the home page
-@app.route('/charts')                                                 #home page
-def charts():
+# @app.route('/charts')                                                 #home page
+# def charts():
     
-    return render_template('charts.html')
+#     return render_template('charts.html')
 
-#takes you to the home page
-@app.route('/tables')                                                 #home page
-def tables():
-    return render_template('tables.html')
+# @app.route('/tables')                                                 #home page
+# def tables():
+#     return render_template('tables.html')
 
 #if this were the main module, run the application
 if __name__ == '__main__':
