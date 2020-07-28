6aa1c4db169f32f22b961c035553ac74f7206399
am16br <65733333+am16br@users.noreply.github.com>
2020-07-28 14:07:23 -0400

    Update app.py

--- Website/app.py
+++ Website/app.py
@@ -20,36 +20,41 @@ app = Flask(__name__, instance_relative_config=True)
 #Default port is '127.0.0.1:5000'
 
 #takes you to the home page
-@app.route('/')                                                 #home page
+@app.route('/', methods = ['POST','GET'])                                                 #home page
 def home():
-    ticker = '^GSPC'                #setting up ticker for file names and usage
+    if request.method == 'GET':
+        ticker = '^GSPC'
+    if request.method == 'POST':
+        ticker = request.form["ticker"]
     csvname = (ticker + '.csv')
     dbname = ('Projecto.db')
     labels = []
     values = []
 
-    if os.path.exists(csvname):     #ensuring csv has newest data
+    if os.path.exists(csvname):
       os.remove(csvname)
 
+    #years = int(input("Enter number of years to check stock data: "))
     endDate = date.today()
     startDate = date(endDate.year - 1, endDate.month, endDate.day)
 
-    df = web.DataReader(ticker, 'yahoo', startDate, endDate)    #scraping stock data from web fpr given timeframe
+    df = web.DataReader(ticker, 'yahoo', startDate, endDate)
     df.to_csv(csvname)
- 
+
     con = sqlite3.connect(dbname)                   #connecting to/creating/opening database
     con.row_factory = sqlite3.Row
     cur = con.cursor()                              #setting cursor
 
-    cur.execute("""DROP TABLE IF EXISTS STOCK""")   #Delete old table, to avoid duplicates
-        #creating new table for stock with relative data
+    #Delete old table, to avoid duplicates
+    cur.execute("""DROP TABLE IF EXISTS STOCK""")
+
     cur.execute('CREATE TABLE Stock(Date TEXT, High REAL, Low REAL, Open REAL, Close REAL, Volume REAL, AdjClose REAL);')
 
     try:
         with open(csvname) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             next(csv_reader)
-            for row in csv_reader:  #looping through csv to move data to database
+            for row in csv_reader:
                 iterations = 0
                 Date = row[0]
                 High = row[1]
@@ -60,7 +65,7 @@ def home():
                 AdjClose = row[6]
                 cur.execute("""INSERT INTO Stock(Date, High, Low , Open, Close, Volume, AdjClose)
                             VALUES (?, ?, ?, ?, ?, ?, ?);""", (Date, High, Low, Open, Close, Volume, AdjClose))
-                con.commit()    #comminting data to databse
+                con.commit()
 
                 if iterations < 200:
                     labels.append(Date)
@@ -74,18 +79,69 @@ def home():
         print("error in insert operation")
         rows = 'error'
     finally:
-        return render_template('index.html', rows=rows, labels=labels, values=values)
+        return render_template('index.html', ticker=ticker, rows=rows, labels=labels, values=values)
+
 
 #if index is typed directly it redirects to '/'
-@app.route('/index', methods=['GET'])                           #takes you to the route
+@app.route('/index', methods=['GET','POST'])                           #takes you to the route
 def index():
     return redirect(url_for('home'))
+@app.route('/stock', methods=['POST'])
+def stock():
+    ticker = request.form["ticker"]
+    csvname = (ticker + '.csv')
+    dbname = ('Projecto.db')
+    labels = []
+    values = []
+    if os.path.exists(csvname):
+      os.remove(csvname)
 
-# @app.route('/charts')                                                 #home page
-# def charts():
-    
-#     return render_template('charts.html')
+    #years = int(input("Enter number of years to check stock data: "))
+    endDate = date.today()
+    startDate = date(endDate.year - 1, endDate.month, endDate.day)
 
+    df = web.DataReader(ticker, 'yahoo', startDate, endDate)
+    df.to_csv(csvname)
+
+    con = sqlite3.connect(dbname)                   #connecting to/creating/opening database
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()                              #setting cursor
+
+    #Delete old table, to avoid duplicates
+    cur.execute("""DROP TABLE IF EXISTS STOCK""")
+
+    cur.execute('CREATE TABLE Stock(Date TEXT, High REAL, Low REAL, Open REAL, Close REAL, Volume REAL, AdjClose REAL);')
+
+    try:
+        with open(csvname) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            next(csv_reader)
+            for row in csv_reader:
+                iterations = 0
+                Date = row[0]
+                High = row[1]
+                Low = row[2]
+                Open = row[3]
+                Close = row[4]
+                Volume = row[5]
+                AdjClose = row[6]
+                cur.execute("""INSERT INTO Stock(Date, High, Low , Open, Close, Volume, AdjClose)
+                            VALUES (?, ?, ?, ?, ?, ?, ?);""", (Date, High, Low, Open, Close, Volume, AdjClose))
+                con.commit()
+
+                if iterations < 200:
+                    labels.append(Date)
+                    # print('row 6 is ', row[6])
+                    values.append(row[6])
+                    iterations += 1
+        cur.execute('SELECT * FROM Stock')
+        rows = cur.fetchall();
+    except:
+        con.rollback()                                      #in the event of an error rollback database
+        print("error in insert operation")
+        rows = 'error'
+    finally:
+        return render_template('index.html', ticker=ticker, rows=rows, labels=labels, values=values)
 # @app.route('/tables')                                                 #home page
 # def tables():
 #     return render_template('tables.html')
