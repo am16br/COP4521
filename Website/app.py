49e4899a4e84b9b4b384b627711e1d5b8f676031
Xander0070 <50340350+Xander0070@users.noreply.github.com>
2020-07-28 17:17:35 -0400

    simplified pages

--- Website/app.py
+++ Website/app.py
@@ -45,39 +45,38 @@ app = Flask(__name__, instance_relative_config=True)
 #Default port is '127.0.0.1:5000'
 
 #takes you to the home page
-@app.route('/', methods = ['POST','GET'])                                                 #home page
+@app.route('/')                                                 #home page
 def home():
-    if request.method == 'GET':     #initially loads S&P500
-        ticker = '^GSPC'
-    if request.method == 'POST':    #loads new stock
-        ticker = request.form["ticker"]
-    csvname = (ticker + '.csv')     #creating file to hold data
-    dbname = ('Projecto.db')        #creating database
+    ticker = '^GSPC'
+    csvname = (ticker + '.csv')
+    dbname = ('Projecto.db')
     labels = []
     values = []
 
-    if os.path.exists(csvname):     #ensuring data is most recent
+    if os.path.exists(csvname):
       os.remove(csvname)
-    endDate = date.today()          #setting dates to load data from
+
+    #years = int(input("Enter number of years to check stock data: "))
+    endDate = date.today()
     startDate = date(endDate.year - 1, endDate.month, endDate.day)
 
-    df = web.DataReader(ticker, 'yahoo', startDate, endDate)    #scraping stock data from web for range of dates
+    df = web.DataReader(ticker, 'yahoo', startDate, endDate)
     df.to_csv(csvname)
-
+ 
     con = sqlite3.connect(dbname)                   #connecting to/creating/opening database
     con.row_factory = sqlite3.Row
     cur = con.cursor()                              #setting cursor
 
     #Delete old table, to avoid duplicates
     cur.execute("""DROP TABLE IF EXISTS STOCK""")
-    #create table to hold relevant data
+
     cur.execute('CREATE TABLE Stock(Date TEXT, High REAL, Low REAL, Open REAL, Close REAL, Volume REAL, AdjClose REAL);')
 
     try:
         with open(csvname) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             next(csv_reader)
-            for row in csv_reader:  #moving data from csv to database
+            for row in csv_reader:
                 iterations = 0
                 Date = row[0]
                 High = row[1]
@@ -88,7 +87,7 @@ def home():
                 AdjClose = row[6]
                 cur.execute("""INSERT INTO Stock(Date, High, Low , Open, Close, Volume, AdjClose)
                             VALUES (?, ?, ?, ?, ?, ?, ?);""", (Date, High, Low, Open, Close, Volume, AdjClose))
-                con.commit()    #committing data to database
+                con.commit()
 
                 if iterations < 200:
                     labels.append(Date)
@@ -102,29 +101,16 @@ def home():
         print("error in insert operation")
         rows = 'error'
     finally:
-        min=float(values[0])        #setting min and max values for y-axis of chart
-        max=float(values[0])
-        for item in values:         #modifying min and max values from list of stock values
-            if float(item)>max:
-                max=float(item)
-            if float(item)<min:
-                min=float(item)
-        min=min-(min*0.25)
-        max=max+(max*0.25)
-        if ticker=="^GSPC":
-            ticker="S&P500"
-        return render_template('index.html', ticker=ticker, rows=rows, labels=labels, values=values, min=min, max=max)
-        con.close()         #closing connection
+        return render_template('index.html', rows=rows, labels=labels, values=values)
 
 #if index is typed directly it redirects to '/'
 @app.route('/index', methods=['GET','POST'])                           #takes you to the route
 def index():
     return redirect(url_for('home'))
 
-@app.route('/stock', methods=['POST'])
+@app.route('/stock', methods=['GET','POST'])
 def stock():
-    #similar to index for individual stocks
-    ticker = request.form["ticker"]
+    ticker = request.form["ticker"]   
     csvname = (ticker + '.csv')
     dbname = ('Projecto.db')
     labels = []
@@ -194,7 +180,7 @@ def portfolio():
     #  ticker = request.form["ticker"]
     #  qty = request.form["qty"]
     #  purprice = request.form["price"]
-     portfolio.append(Stock(ticker,qty,purprice))
+    #  portfolio.append(Stock(ticker,qty,purprice))
      inv = 0
      val = 0
      con = sqlite3.connect("Projecto.db")                   #connecting to/creating/opening database
@@ -224,6 +210,7 @@ def portfolio():
          print("error in insert operation")
          rows = 'error'
      finally:
+        #  return render_template('portfolio.html')
          return render_template('portfolio.html', rows=rows, investment=inv, value=val, growth=growth)
          con.close()
 # @app.route('/tables')                                                 #home page
